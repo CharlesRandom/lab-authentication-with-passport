089f39452fbb43bc25a0c1a01d0e61feb9ada5ce
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-26 14:43:00 -0600

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const passport     = require("./helpers/passport")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")(session)
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/auth-passjs', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -30,6 +33,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(session({
+  secret: process.env.SECRET,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  })
+}))
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -45,9 +56,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
+//después de views ponemos passport
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Lab - PassportJS';
 
 
 // Routes middleware goes here
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,14 +1,40 @@
 const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
+const User = require("../models/User")
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require('bcrypt')
+const saltRounds = 7
 
 // Add passport 
+const passport = require("passport")
 
 
 const ensureLogin = require("connect-ensure-login");
 
+passportRouter.get('/signup', (req, res, next)=>{
+  res.render('passport/signup')
+})
+
+passportRouter.post('/signup',(req, res, next) => {
+  //register es del plugin, el modelo lo tiene porque se le agregó
+  console.log(req.body)
+  User.register(req.body, req.body.password)
+  .then(user=>{
+    res.json(user)
+  })
+  .catch(e => next(e))
+})
+
+passportRouter.post('/login', passport.authenticate("local"), (req, res, next)=>{
+  const username = req.user.username
+  res.send('Tu eres un usuario real con username: ' + username)
+})
+
+passportRouter.get('/login',(req, res, next)=>{
+  res.render('passport/login')
+})
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
